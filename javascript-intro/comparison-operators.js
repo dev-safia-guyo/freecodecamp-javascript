c2cd4655d50c26d9451fc9dbb308576ec343768e
dev-safia-guyo <safiaguy444@gmail.com>
2026-05-22 09:33:01 +0300

    feat: added a new file

---
+++ javascript-intro/comparison-operators.js
@@ -0,0 +1,50 @@
+if (0) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}
+if ("") {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}	
+if ('0') {
+	console.log("Truthy");
+} else {
+	console.log("Falsy")
+}	
+if ([]) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}
+if (null) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}	
+if (-1) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}	
+if (undefined) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}
+if ('False') {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}  
+if (NaN) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}
+if ({}) {
+	console.log("Truthy");
+} else {
+	console.log("Falsy");
+}	
