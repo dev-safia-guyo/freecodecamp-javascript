ab1732f861a599a3ce0c1427501928874dace84f
dev-safia-guyo <safiaguy444@gmail.com>
2026-06-14 15:06:20 +0300

    feat: added a new file

--- javascript-intro/math-object.js
+++ javascript-intro/math-object.js
@@ -8,5 +8,14 @@ const c = Math.hypot(3, 4);
 console.log(c);
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber);
+const largestNumbers= Math.max(18, 7, 42, 3, 29);
+console.log(largestNumbers);
+const absoluteDifference = Math.abs(-17.5 - 9.3);
+console.log(absoluteDifference);
+const d = 4;
+const e = 6;
+const f = Math.hypot(4, 6);
+console.log(f);
 
-	
+
+
--- javascript-intro/number-conversion.js
+++ javascript-intro/number-conversion.js
@@ -9,3 +9,8 @@ console.log(resultC);
 const price = 1234.5678;
 const resultD = price.toFixed(2);
 console.log(resultD);
+const randomNumBtw1And20 = Math.floor(Math.random() * 20) + 1;
+console.log(randomNumBtw1And20);
+const random = Math.floor(Math.random() * 20) + 1;
+console.log(random);
+
