8e1d45986688be38f0dd08986b6f3097e0467306
dev-safia-guyo <safiaguy444@gmail.com>
2026-06-14 15:25:12 +0300

    feat: added a new file

---
+++ javascript-intro/logical-operator.js
@@ -0,0 +1,7 @@
+console.log(true && 'hello');
+console.log(false && 'hello');
+console.log(0   && 'hello');
+console.log('' || 'default');
+console.log('Alice' || 'Bob');
+console.log(false || null || 0 || 'found');
+console.log(1 && 2 && 3);
