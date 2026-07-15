735285566aefcf339d4f0cfde0b572c6ed5ca71c
dev-safia-guyo <safiaguy444@gmail.com>
2026-07-15 16:33:22 +0300

    feat: added a new file

---
+++ switch-statement.js
@@ -0,0 +1,11 @@
+const day =4 
+switch (day) {
+	case 1: console.log('Monday'); break;
+	case 2: console.log('Tuesday'); break;
+	case 3: console.log('Wednesday'); break;
+	case 4: console.log('Thursday'); break; 	
+        case 5: console.log('Friday'); break;
+	case 6:
+	case7: console.log('weekend'); break;
+	default: console.log('Invalid day number');
+}		
